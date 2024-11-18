d246d8a10d696fff7feacc080d6fac07643c0185
♥ﮩ٨ـGriezman2003ﮩ٨ـ♥ <102940734+Griezman2003@users.noreply.github.com>
2024-11-17 22:33:16 -0600

    mispell

--- app/redes/redes.js
+++ app/redes/redes.js
@@ -1,16 +1,18 @@
 import { Linking } from "react-native";
 
-export const facebook = () => {
+const facebook = () => {
     Linking.openURL('https://facebook.com')
     .catch((error) => console.error('Error al abrir el enlace: ', error));
 };
 
 
-export const google = () => {
+const google = () => {
     Linking.openURL('https://google.com')
     .catch((error) => console.error('Error al abrir el enlace: ', error));
 };
 
-export const x = () =>{
+const x = () =>{
     Linking.openURL('https://x.com').catch((error)=> console.log('Error al abrir el enlace: ', error))
 }
+
+export {facebook, google, x}
