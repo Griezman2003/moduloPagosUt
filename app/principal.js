e6659d51fd6f42263c94c0d37f315ce967ba8324
♥ﮩ٨ـGriezman2003ﮩ٨ـ♥ <102940734+Griezman2003@users.noreply.github.com>
2024-11-18 00:22:36 -0600

    implementacion del sidebar

--- app/principal.js
+++ app/principal.js
@@ -1,27 +1,62 @@
-import {ScrollView, View, Text, StyleSheet, TouchableOpacity, Pressable} from "react-native";
-import { Avatar, Card, Button } from "react-native-paper";
+import React, { useState, useRef } from 'react';
+import { ScrollView, View, Text, StyleSheet, TouchableOpacity, Pressable, Animated, Dimensions } from 'react-native';
+import { Avatar, Card, Drawer } from "react-native-paper";
 import { useNavigation } from '@react-navigation/native';
 import { FontAwesome } from '@expo/vector-icons'; 
 import AntDesign from '@expo/vector-icons/AntDesign';
 import { dato, targetas } from "./json/json";
+import  Sidebar  from "./sidebar";
 
-
-export default function Principal (){
+export default function Principal() {
     const navigation = useNavigation();
+    
+    const [isSidebarVisible, setIsSidebarVisible] = useState(false);
+    const slideAnim = useRef(new Animated.Value(-Dimensions.get('window').width * 0.6)).current;
+    
+    const toggleSidebar = () => {
+        const toValue = isSidebarVisible ? -Dimensions.get('window').width * 0.6 : 0;
+        Animated.timing(slideAnim, {
+            toValue,
+            duration: 300,
+            useNativeDriver: true,
+        }).start();
+        setIsSidebarVisible(!isSidebarVisible);
+    };
+    const closeSidebar = () => {
+        Animated.timing(slideAnim, {
+            toValue: -Dimensions.get('window').width * 0.6,
+            duration: 300,
+            useNativeDriver: true,
+        }).start();
+        setIsSidebarVisible(false);
+    };
     return (
         <ScrollView style={style.container} showsVerticalScrollIndicator={false}>
+        
+        {isSidebarVisible && (
+            <TouchableOpacity style={style.overlay} onPress={closeSidebar} />
+        )}
+
+        {/* Sidebar */}
+        <Sidebar slideAnim={slideAnim} />
+        {/* fin del sidebar */}
+        
+        {/* contenido general */}
         <View style={style.header}>
+        <Pressable onPress={toggleSidebar}>
         <AntDesign name="menu-fold" size={35} color="white" />
-        <TouchableOpacity onPress={()=>navigation.navigate('perfil')}>
+        </Pressable>
+        
+        <TouchableOpacity onPress={() => navigation.navigate('perfil')}>
         <FontAwesome name="user" size={35} color="white" />
         </TouchableOpacity>
         </View>
         <View style={style.balance}>
-        <Text style={{color:'white', fontSize:12}}>Total de Balance</Text>
-        <Text style={{color:'white', fontSize:30}}>$3,831.59</Text>
+        <Text style={{ color: 'white', fontSize: 12 }}>Total de Balance</Text>
+        <Text style={{ color: 'white', fontSize: 30 }}>$3,831.59</Text>
         </View>
-        {/* ----------------------------------------------------------------------------------------- */}
         
+        {/* Tarjetas */}
         <ScrollView style={{ marginTop: 30, height: 250 }} horizontal={true} showsHorizontalScrollIndicator={false}>
         <View style={{ flexDirection: 'row' }}>
         {targetas.map((item) => (
@@ -54,72 +89,89 @@ export default function Principal (){
         </View>
         </ScrollView>
         
-        
-        {/* ------------------------------------------------------------------------------------------------ */}
-        
-        <View style={{marginTop:50, display:'flex', justifyContent:'center', alignItems:'center',}}>
-        <View style={{display:'flex', justifyContent:'center', alignItems:'center', flexDirection:'row', gap:50,
-            backgroundColor:'#5C34A6', width:265, height:65, borderRadius:99999,
-        }}>
+        {/* Botones de ingresos y egresos */}
+        <View style={{ marginTop: 50, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <View
+        style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'row',
+            gap: 50,
+            backgroundColor: '#5C34A6',
+            width: 265,
+            height: 65,
+            borderRadius: 99999,
+        }}
+        >
         <TouchableOpacity>
-        <View style={{backgroundColor:'#000', padding:10, borderRadius:20}}>
-        <Text style={{color:'white', fontSize:20}}>
-        Ingresos
-        </Text>
+        <View style={{ backgroundColor: '#000', padding: 10, borderRadius: 20 }}>
+        <Text style={{ color: 'white', fontSize: 20 }}>Ingresos</Text>
         </View>
         </TouchableOpacity>
         
-        <Pressable onPress={()=> navigation.navigate('egreso')}>
+        <Pressable onPress={() => navigation.navigate('egreso')}>
         <View>
-        <Text style={{color:'white', fontSize:20}}>
-        Egresos
-        </Text>
+        <Text style={{ color: 'white', fontSize: 20 }}>Egresos</Text>
         </View>
         </Pressable>
         </View>
         </View>
         
-        {/* -------------------------------------------------------------------------------------------------------- */}
-        
-        <View style={{ marginTop: 50, paddingHorizontal:10 }}>
+        {/* Lista de datos */}
+        <View style={{ marginTop: 50, paddingHorizontal: 10 }}>
         {dato.map((item) => (
-            <View key={item.id}  style={{ flexDirection: 'row', marginVertical: 20, justifyContent: 'space-between',
-                alignItems:'center', paddingHorizontal:15,
-            }}>
-            <View style={{ flexDirection: 'row', gap: 10, }}>
+            <View
+            key={item.id}
+            style={{
+                flexDirection: 'row',
+                marginVertical: 20,
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                paddingHorizontal: 15,
+            }}
+            >
+            <View style={{ flexDirection: 'row', gap: 10 }}>
             <Avatar.Image size={40} source={require('../assets/images/react-logo.png')} />
             <View>
-            <Text style={{ color: 'white', fontSize:15 }}>{item.nombre}</Text>
-            <Text style={{ color: 'white', fontSize:15 }}>{item.fecha}</Text>
+            <Text style={{ color: 'white', fontSize: 15 }}>{item.nombre}</Text>
+            <Text style={{ color: 'white', fontSize: 15 }}>{item.fecha}</Text>
             </View>
             </View>
             <Text style={{ color: 'green' }}>{item.monto}</Text>
             </View>
         ))}
         </View>
-        
         </ScrollView>
     );
-};
+}
 
 const style = StyleSheet.create({
-    container:{
-        paddingVertical:35,
-        backgroundColor:'#100F1F',
+    container: {
+        paddingVertical: 35,
+        backgroundColor: '#100F1F',
     },
-    header:{
-        display:'flex',
-        flexDirection:'row',
-        justifyContent:'center',
-        alignItems:'center',
-        gap:280,
+    header: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 280,
     },
-    balance:{
-        display:'flex',
-        alignItems:'center',
-        justifyContent:'center',
-        marginTop:30,
-        gap:5
+    balance: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 30,
+        gap: 5,
     },
-})
-
+    overlay: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        backgroundColor: 'rgba(0, 0, 0, 0.5)', 
+        zIndex: 0,
+    }
+});
---
+++ app/sidebar.js
@@ -0,0 +1,48 @@
+// Sidebar.js
+import React from 'react';
+import { Animated, StyleSheet, Dimensions } from 'react-native';
+import { Drawer } from 'react-native-paper';
+import { useNavigation } from '@react-navigation/native';
+
+const Sidebar = ({ slideAnim }) => {
+    const navigation = useNavigation();
+    return (
+        <Animated.View
+        style={[
+            styles.sidebar,
+            {
+                transform: [{ translateX: slideAnim }],
+            },
+        ]}
+        >
+        <Drawer.Section title="Pago ut">
+        <Drawer.Item
+        label="Inicio"
+        icon="star"
+        style={{backgroundColor: '#64ffda' }}
+        onPress={() => navigation.navigate('index')}
+        />
+        <Drawer.Item
+        label="Perfil"
+        icon="star"
+        style={{backgroundColor: '#64ffda', marginVertical:20 }} 
+        onPress={() => {}}
+        />
+        </Drawer.Section>
+        </Animated.View>
+    );
+};
+
+const styles = StyleSheet.create({
+    sidebar: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        width: '60%',
+        backgroundColor: '#100F1F', 
+        zIndex: 2, 
+    },
+});
+
+export default Sidebar;
