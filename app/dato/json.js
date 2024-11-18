e21509b08c3093602e697e5c03111d208ca931bf
♥ﮩ٨ـGriezman2003ﮩ٨ـ♥ <102940734+Griezman2003@users.noreply.github.com>
2024-11-17 20:07:39 -0600

    datos separados a archivo

---
+++ app/dato/json.js
@@ -0,0 +1,53 @@
+
+    const data = [
+        { id: 1, 
+            nombre: 'Blurryface 125', 
+            fecha: '17/11/2024', 
+            monto: '$12500' 
+        },
+        { id: 2, 
+            nombre: 'Blurryface 126', 
+            fecha: '18/11/2024', 
+            monto: '$8500' 
+        },
+        { id: 3, 
+            nombre: 'Blurryface 127', 
+            fecha: '19/11/2024', 
+            monto: '$650' 
+        },
+        { id: 4, 
+            nombre: 'Blurryface 128', 
+            fecha: '20/11/2024', 
+            monto: '$4500' },
+        { id: 5, 
+                nombre: 'Blurryface 129', 
+                fecha: '21/11/2024', 
+                monto: '$1050' },
+        ];
+            
+    const targetas = [
+        {id: 1, 
+            date: '17/2024',
+            cardNumber: '1478 2265 4595 9874',
+            name: 'Gamaliel Garcia',
+            cardType: 'Mastercard',
+            imageSource: require('../../assets/images/mastercard.jpg'),
+        },
+        {id:2,
+            date: '18/2024',
+            cardNumber: '2345 6789 1234 5678',
+            name: 'Maria Lopez',
+            cardType: 'Visa',
+            imageSource: require('../../assets/images/mastercard.jpg'),
+        },
+        {
+            id:3,
+            date: '19/2024',
+            cardNumber: '2345 6789 1234 5678',
+            name: 'Juan lopez',
+            cardType: 'Mastercard',
+            imageSource: require('../../assets/images/mastercard.jpg'),
+        }
+        ];
+
+        export { data, targetas };
--- app/principal.js
+++ app/principal.js
@@ -3,58 +3,9 @@ import { Avatar, Card, Button } from "react-native-paper";
 import { FontAwesome } from '@expo/vector-icons'; 
 import AntDesign from '@expo/vector-icons/AntDesign';
 import { LinearGradient } from 'expo-linear-gradient';
+import { data, targetas } from "../app/dato/json";
 
 export default function Principal (){
-    const data = [
-        { id: 1, 
-            nombre: 'Blurryface 125', 
-            fecha: '17/11/2024', 
-            monto: '$12500' 
-        },
-        { id: 2, 
-            nombre: 'Blurryface 126', 
-            fecha: '18/11/2024', 
-            monto: '$8500' 
-        },
-        { id: 3, 
-            nombre: 'Blurryface 127', 
-            fecha: '19/11/2024', 
-            monto: '$650' 
-        },
-        { id: 4, 
-            nombre: 'Blurryface 128', 
-            fecha: '20/11/2024', 
-            monto: '$4500' },
-        { id: 5, 
-                nombre: 'Blurryface 129', 
-                fecha: '21/11/2024', 
-                monto: '$1050' },
-        ];
-            
-    const targetas = [
-        {id: 1, 
-            date: '17/2024',
-            cardNumber: '1478 2265 4595 9874',
-            name: 'Gamaliel Garcia',
-            cardType: 'Mastercard',
-            imageSource: require('../assets/images/mastercard.jpg'),
-        },
-        {id:2,
-            date: '18/2024',
-            cardNumber: '2345 6789 1234 5678',
-            name: 'Maria Lopez',
-            cardType: 'Visa',
-            imageSource: require('../assets/images/mastercard.jpg'),
-        },
-        {
-            id:3,
-            date: '19/2024',
-            cardNumber: '2345 6789 1234 5678',
-            name: 'Juan lopez',
-            cardType: 'Mastercard',
-            imageSource: require('../assets/images/mastercard.jpg'),
-        }
-        ];
             return (
                 
                 <ScrollView style={style.container} showsVerticalScrollIndicator={false}>
